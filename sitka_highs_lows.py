a5aaed3d2093dcd982b6f4e51c8acd07ae88a074
Ambush3 <aaronbush3@gmail.com>
2021-09-01 19:09:03 -0400

    Initial commit

---
+++ sitka_highs_lows.py
@@ -0,0 +1,35 @@
+import csv
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+# Get dates, high, and low temperatures from file.
+filename = 'F:/PyCharm/Mapping Global Earthquakes/data/world_fires_7_day.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    # Get dates, and high and low temps from this file.
+    dates, highs, lows = [], [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        high = int(row[5])
+        low = int(row[6])
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
+
+# Plot the high and low temperatures.
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+# Format plot.
+plt.title("World Fires", fontsize=24)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Brightness(F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
